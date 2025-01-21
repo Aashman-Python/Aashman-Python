23ca75d3210cfa502af08fc4e57ab73e51d6399d
Aashman-Python <aashmanpython@gmail.com>
2025-01-21 16:52:07 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,30 @@
+# Declare a variable x
+x = 10
+
+# Print the variable x
+print(x)
+
+# Output: 10
+
+
+#You can also perform operations with x:
+
+
+x = 10
+y = 5
+
+# Print the sum of x and y
+print(x + y)
+
+# Output: 15
+
+
+#Or use x in a sentence:
+
+
+x = "Aashman"
+
+# Print a sentence with x
+print("Hello, my name is " + x)
+
+# Output: Hello, my name is Aashman
